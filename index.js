a8224e8bfddd0fe4a510315c576e9574f270b070
Zerin Tasnim <zerintasnimriana@gmail.com>
2024-02-18 15:33:04 +0600

    feat: project setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./config");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", Router);
+
+(async function () {
+  try {
+    await mongoose.connect(config.MONGOOSE_URI);
+    console.log("[mongoose]: Connected to DB.");
+    app.listen(config.PORT, () =>
+      console.log(`[server]: Server is listening on port ${config.PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+})();
